7189cc7c14c6b652a7354b751603b7bcd7aa180d
Castus Info <castusinfo@gmail.com>
2019-07-26 11:14:47 +0530

    added files

---
+++ src/app/business.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BusinessService {
+
+  uri = 'http://localhost:4000/business';
+  versionuri = 'http://localhost:4000/getversion';
+
+  constructor(private http: HttpClient) { }
+
+  addBusiness(person_name, business_name, business_gst_number) {
+    const obj = {
+      person_name: person_name,
+      business_name: business_name,
+      business_gst_number: business_gst_number
+    };
+    return this.http.post(`${this.uri}/add`, obj);
+  }
+
+  getBusinesses() {
+    return this
+           .http
+           .get(`${this.uri}`);
+  }
+
+  getVersion() {
+    return this
+           .http
+           .get(`${this.versionuri}`);
+  }
+
+  editBusiness(id) {
+    return this
+            .http
+            .get(`${this.uri}/edit/${id}`);
+    }
+
+  updateBusiness(person_name, business_name, business_gst_number, id) {
+
+    const obj = {
+        person_name: person_name,
+        business_name: business_name,
+        business_gst_number: business_gst_number
+      };
+   return this
+      .http
+      .post(`${this.uri}/update/${id}`, obj);
+  }
+
+ deleteBusiness(id) {
+    return this
+              .http
+              .get(`${this.uri}/delete/${id}`);
+  }
+}
